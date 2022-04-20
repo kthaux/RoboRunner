6b71807132df361a10c9ff711ac650bec2103ec8
Knackington <keaton.haux@gmail.com>
2022-04-20 12:41:05 -0700

    Initial commit. index.html, main.js and Menu.js created

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("hello from main.js");
+//basic main.js copied over from the rocket patrol game
+let config = 
+{
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play]
+}
+
+let game = new Phaser.Game(config);
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+borderUISize = game.config.height / 15;
+borderPadding = borderUISize / 3;
